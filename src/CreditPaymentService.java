13cd5b08ef0d82e1f8f55003f6b5d12e08e9e354
Anton Titov <anton.titov97@gmail.com>
2023-03-16 21:47:46 +0300

    credit.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public int calculate(int period, int amount, double rate) {
-        double monthPercent = rate / 100 / period;
+        double monthPercent = rate / 100 / 12;
         double coefficient = (monthPercent * (Math.pow(1 + monthPercent, period) / ((Math.pow(1 + monthPercent, period)) - 1)));
         double payment = coefficient * amount;
         return (int) payment;
--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         double rate = 9.99;
         int payment = service.calculate(period, amount, rate);
         System.out.println(payment);
+        System.out.println(service.calculate(24, amount, rate));
+        System.out.println(service.calculate(36, amount, rate));
     }
 }
